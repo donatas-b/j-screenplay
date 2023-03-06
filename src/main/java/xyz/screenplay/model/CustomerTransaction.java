4f9bac61b547d767b63e33182626ac3f54d21477
Donatas Barkauskas <donatas.barkauskas@sage.com>
2023-03-06 16:06:17 +0200

    Customer can check Transactions which happened in his Account

---
+++ src/main/java/xyz/screenplay/model/CustomerTransaction.java
@@ -0,0 +1,26 @@
+package xyz.screenplay.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class CustomerTransaction {
+    private String dateTime;
+    private Integer amount;
+    private TransactionType transactionType;
+
+    public CustomerTransaction(Map<Object, String> tableRow) {
+        this.dateTime = tableRow.get(1);
+        this.amount = Integer.valueOf(tableRow.get(2));
+        this.transactionType = TransactionType.byValue(tableRow.get(3));
+    }
+
+    public String toStringNoDate() {
+        return String.format("CustomerTransaction(amount=%s, transactionType=%s)", this.amount, this.transactionType);
+    }
+}
--- src/test/java/xyz/screenplay/stepdefinitions/CustomerSteps.java
+++ src/test/java/xyz/screenplay/stepdefinitions/CustomerSteps.java
@@ -1,5 +1,6 @@
 package xyz.screenplay.stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,11 +8,18 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import xyz.screenplay.model.AccountSummary;
 import xyz.screenplay.model.Currency;
+import xyz.screenplay.model.CustomerTransaction;
+import xyz.screenplay.model.TransactionType;
 import xyz.screenplay.questions.CustomerAccount;
 import xyz.screenplay.questions.CustomerSuccess;
+import xyz.screenplay.questions.CustomerTransactions;
 import xyz.screenplay.tasks.Customer;
 import xyz.screenplay.tasks.Login;
 import xyz.screenplay.tasks.Navigate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
@@ -60,5 +68,23 @@ public class CustomerSteps {
     public void customerResetsHisAccount(Actor actor, String accountNumber) {
         actor.attemptsTo(Customer.selectAccount(accountNumber));
         actor.attemptsTo(Customer.resetAccount());
+    }
+
+    @Then("{actor} {string} Account Transactions should contain following records")
+    public void hisAccountTransactionsShouldContainFollowingRecords(Actor actor, String accountNumber, DataTable expectedRecords) {
+        actor.attemptsTo(Customer.selectAccount(accountNumber));
+        actor.attemptsTo(Customer.transactions());
+
+        List<CustomerTransaction> expectedTransactions = new ArrayList<>();
+        List<Map<String, String>> rows = expectedRecords.asMaps(String.class, String.class);
+        for (Map<String, String> columns : rows) {
+            expectedTransactions.add(CustomerTransaction.builder()
+                    .amount(Integer.valueOf(columns.get("Amount")))
+                    .transactionType(TransactionType.byValue(columns.get("TransactionType")))
+                    .build());
+        }
+        List<String> actualTransactions = CustomerTransactions.all().answeredBy(actor).stream().map(CustomerTransaction::toStringNoDate).toList();
+        ;
+        expectedTransactions.forEach(expectedTransaction -> actor.attemptsTo(Ensure.that(actualTransactions).contains(expectedTransaction.toStringNoDate())));
     }
 }
