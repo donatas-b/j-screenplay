a6a50ac8f569a9eede781280e06f8e2bca4d5a55
donatas-b <45396946+donatas-b@users.noreply.github.com>
2023-03-22 15:09:46 +0200

    maven.yml (#29)
    github action

--- src/main/java/xyz/screenplay/tasks/LoginAsCustomer.java
+++ src/main/java/xyz/screenplay/tasks/LoginAsCustomer.java
@@ -4,10 +4,15 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.SelectFromOptions;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import xyz.screenplay.userInterface.CustomerHomePage;
 import xyz.screenplay.userInterface.CustomerLoginPage;
 import xyz.screenplay.userInterface.LoginPage;
 
+import java.time.Duration;
+
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class LoginAsCustomer extends LoginAsCustomerBase implements Task {
 
@@ -19,7 +24,8 @@ public class LoginAsCustomer extends LoginAsCustomerBase implements Task {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Click.on(LoginPage.BTN_CUSTOMER_LOGIN),
                 SelectFromOptions.byVisibleText(customer(actor).getUser().fullName()).from(CustomerLoginPage.DRP_YOUR_NAME),
-                Click.on(CustomerLoginPage.BTN_LOGIN));
+                Click.on(CustomerLoginPage.BTN_LOGIN),
+                WaitUntil.the(CustomerHomePage.TXT_ACCOUNT_SUMMARY, isVisible()).forNoMoreThan(Duration.ofSeconds(3)));
     }
 
 }
